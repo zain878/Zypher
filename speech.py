9d2c96ab3ab67ffd537e90fc5eb5a690040b8805
zain878 <zainali43257@gmail.com>
2026-08-21 04:36:23 +0500

    JACK/ALSA Notifications turned off

--- speech.py
+++ speech.py
@@ -1,33 +1,65 @@
 import speech_recognition as sr
 import pyttsx3 as psx
+from ctypes import *
+import contextlib
+
+import os
+import sys
+import contextlib
+
+@contextlib.contextmanager
+def suppress_stderr():
+    stderr_fd = sys.stderr.fileno()
+
+    # Save original stderr
+    saved_stderr = os.dup(stderr_fd)
+
+    with open(os.devnull, "w") as devnull:
+        # Redirect stderr to /dev/null
+        os.dup2(devnull.fileno(), stderr_fd)
+
+        try:
+            yield
+        finally:
+            # Restore stderr
+            os.dup2(saved_stderr, stderr_fd)
+            os.close(saved_stderr)
 
 r = sr.Recognizer()
 engine = psx.init()
 
 def listenAudio():
 
-    with sr.Microphone() as source:
-        print("Speak Now")
-        r.adjust_for_ambient_noise(source, duration=1)
+    with suppress_stderr():
+        with sr.Microphone() as source:
 
-        try:
-            audio = r.listen(source, timeout=5, phrase_time_limit=10)
-            text = r.recognize_google(audio)
+            print("🎤 Listening...")
 
-        except sr.UnknownValueError:
-            print("Could not hear anything")
-            return None
+            r.adjust_for_ambient_noise(source, duration=0.5)
 
-        except sr.WaitTimeoutError:
-            print("Did not spoke")
-            return None
+            try:
+                audio = r.listen(
+                    source,
+                    timeout=5,
+                    phrase_time_limit=10
+                )
 
-        except sr.RequestError as e:
-            print("Speech recognition service error:", e)
-            return None
+                text = r.recognize_google(audio)
 
-    return text
+                return text.strip()
 
+            except sr.WaitTimeoutError:
+                print("⌛ No speech detected.")
+                return None
+
+            except sr.UnknownValueError:
+                print("❓ Couldn't understand.")
+                return None
+
+            except sr.RequestError:
+                print("🌐 Speech service unavailable.")
+                return None
+            
 def speak(sp):
     engine.say(sp)
     engine.runAndWait()
